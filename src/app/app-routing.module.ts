9f8181c39d7afbccb4e6123929abc035cfa894ee
PtrAless23 <patrickdemelloalessi@gmail.com>
2021-01-14 16:58:33 -0300

    Front-End 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsListComponent } from './products-list/products-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'produtos', pathMatch: 'full' },
+  { path: 'produtos', component: ProductsListComponent },
+  { path: '**', redirectTo: '/404' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/product-rating.ts
@@ -0,0 +1,9 @@
+export class ProductRating {
+
+    id: number;
+    product_id: number;
+    name: string;
+    grade: number;
+    comment: string;
+    
+}
